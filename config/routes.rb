f4298252977a4544b58ac7a111d0c4d3d01b4866
Corbin Page <corbin.page@gmail.com>
2014-05-08 15:29:16 -0400

    Added some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :static_contents
 
+  root to: "static_contents#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
